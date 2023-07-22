b7d06723eca448f2740c7ed12b55257196ee6639
baraaashraf <baraaashraf44@gmail.com>
2023-07-22 17:58:17 +0800

    adding auth

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path
+from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -15,4 +16,26 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+        print(db.create_all)
+        print('Database Created!')
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) 
+
     return app
+
+
+# def createDatabase(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         
